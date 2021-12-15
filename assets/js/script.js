94a89fddbe832296b4292a15f3045a2d9918f506
Emily <emilyadarr@gmail.com>
2021-12-14 22:36:37 -0500

    get requirements function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,84 @@
 // Assignment code here
 
-var specialChars = []
-var lowercaseLetters = []
-var uppercaseLetters = []
-var numeric = []
-var possibleChars = []
+var specialChars = ["!","@","#","$","%","^","&","*","-","+","?"]
+var lowercaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var uppercaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var possibleChars = [""]
 
-
-var passwordLength = function() {
-
-}
-function generatePassword() {
+//ask for requirements
+function getRequirements() {
   var promptPasswordLength = window.prompt("How long would you like your password? Please select a number between 8 and 128.");
   localStorage.setItem("passwordLength", promptPasswordLength)
+  if (promptPasswordLength < 8 || promptPasswordLength > 128) {
+    window.alert("You must enter a number between 8 and 128.");
+    return getRequirements();
+  };
+  if (promptPasswordLength === "" || promptPasswordLength === null) {
+    window.alert("You must enter a number between 8 and 128.");
+    return getRequirements();
+  };
   console.log(promptPasswordLength);
-
-
+  
   var confirmLowercaseLetters = window.confirm("Would you like to include lowercase letters?");
   console.log(confirmLowercaseLetters);
-
-  var confirmUpercaseLetters = window.confirm("Would you like to include uppercase letters?");
-  console.log(confirmUpercaseLetters);
-
+  
+  var confirmUppercaseLetters = window.confirm("Would you like to include uppercase letters?");
+  console.log(confirmUppercaseLetters);
+  
   var confirmNumeric = window.confirm("Would you like to include numbers?")
   console.log(confirmNumeric);
-
+  
   var confirmSpecialChars = window.confirm("Would you like to include special characters?")
   console.log(confirmSpecialChars);
-}
+
+  if (confirmLowercaseLetters === false && confirmUppercaseLetters === false && confirmNumeric === false && confirmSpecialChars === false) {
+    window.alert("You must select at least one character type.");
+
+    return getRequirements();
+  }
+
+  // object with all password requirements
+  var passwordEntries = {
+    promptPasswordLength: promptPasswordLength,
+    confirmLowercaseLetters: confirmLowercaseLetters,
+    confirmUppercaseLetters: confirmUppercaseLetters,
+    confirmNumeric: confirmNumeric,
+    confirmSpecialChars: confirmSpecialChars,
+  };
+  return passwordEntries;
+};
+
+//generate password
+function generatePassword() {
+  var requirements = getRequirements();
+
+  if (requirements.confirmLowercaseLetters) {
+    possibleChars = possibleChars.concat(lowercaseLetters);
+  }
+
+  if (requirements.confirmUppercaseLetters) {
+    possibleChars = possibleChars.concat(uppercaseLetters);
+  }
+
+  if (requirements.confirmNumeric) {
+    possibleChars = possibleChars.concat(numeric)
+  }
+
+  if (requirements.confirmSpecialChars) {
+    possibleChars = possibleChars.concat(specialChars)
+  }
+  //TODO: LEFT OFF HERE
+  for (var i = 0; i < requirements.promptPasswordLength; i++) {
+    var randomPassword = Math.floor(Math.random() * possibleChars.length);
+    password += possibleChars.substring(randomPassword, randomPassword +1);
+  }
+  console.log(password);
+};
+
+// function getRandom(arr) {
+//   console.log(randomPassword)  
+// }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
